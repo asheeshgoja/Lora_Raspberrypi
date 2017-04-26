847c3e5317c3a14801e5a4fd1ddab8515827f7f8
asheesh goja <asheesh.goja@gmail.com>
2017-04-26 06:09:58 +0000

    init by creator goja

--- examples/raspi/RasPiBoards.h
+++ examples/raspi/RasPiBoards.h
@@ -11,7 +11,7 @@
 #if defined (BOARD_LORASPI)
 #define RF_LED_PIN RPI_V2_GPIO_P1_16 // Led on GPIO23 so P1 connector pin #16
 #define RF_CS_PIN  RPI_V2_GPIO_P1_24 // Slave Select on CE0 so P1 connector pin #24
-#define RF_IRQ_PIN RPI_V2_GPIO_P1_22 // IRQ on GPIO25 so P1 connector pin #22
+//#define RF_IRQ_PIN RPI_V2_GPIO_P1_22 // IRQ on GPIO25 so P1 connector pin #22
 #define RF_RST_PIN RPI_V2_GPIO_P1_15 // IRQ on GPIO22 so P1 connector pin #15
 
 
@@ -66,5 +66,5 @@
 #define RF_LED_PIN NOT_A_PIN				 // No onboard led to drive
 
 #else
-#error "RasPiBoards.h => Please define Hardware Board"
+//#error "RasPiBoards.h => Please define Hardware Board"
 #endif
